09e23f7b0fdfe29942b26954d28edad2e4c0a7cb
Humberto Rocha <humrochagf@gmail.com>
2018-07-07 13:47:19 -0300

    Hero image auto swap

--- apyb/website/views.py
+++ apyb/website/views.py
@@ -1,6 +1,22 @@
+import os
+
+from django.contrib.staticfiles.finders import find
+from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.views.generic import TemplateView
+
+HERO_ROOT = 'website/img/hero'
+HERO_PHOTOS = [
+    static(f'{HERO_ROOT}/{img}') for img in os.listdir(find(HERO_ROOT))
+]
 
 
 class Home(TemplateView):
 
     template_name = 'website/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+
+        context['hero_photos'] = HERO_PHOTOS
+
+        return context
