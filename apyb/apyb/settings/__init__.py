b764813b2d22600c036feaf1efb61bfcd399693b
Humberto Rocha <humrochagf@gmail.com>
2018-08-17 08:55:16 -0300

    Configure testing environment

--- apyb/apyb/settings/__init__.py
+++ apyb/apyb/settings/__init__.py
@@ -1,11 +1,16 @@
-from .common import *
+from .common import *  # noqa
 
 try:
-    from cloudez_settings import *
+    from cloudez_settings import *  # noqa
 except ImportError:
     pass
 
 try:
-    from .local import *
+    from .local import *  # noqa
+except ImportError:
+    pass
+
+try:
+    from .test import *  # noqa
 except ImportError:
     pass
