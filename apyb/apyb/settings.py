b7fb1aca894694298f7900707c28ad1c8dffb134
Artur Gaspar <artur.gaspar.00@gmail.com>
2018-10-22 17:44:56 -0300

    Login and reset password functionality.

--- apyb/apyb/settings.py
+++ apyb/apyb/settings.py
@@ -163,3 +163,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(MEDIA_ROOT, 'static')
+
+
+LOGIN_URL = '/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
