cf32ddcf1c2fbd710df97011d1b8a59571d6a6a7
Rafael Paixão <rafaelsspaixao@gmail.com>
2018-10-22 15:15:29 -0300

    add markdown example

---
+++ apyb/website/markdown/about.md
@@ -0,0 +1,5 @@
+# A APyB
+
+A Associação Python Brasil (APyB) foi formada em abril de 2007 com a meta de apoiar as comunidades relacionadas à linguagem Python e suas tecnologias derivadas.
+
+Sua atual diretoria atuará até Setembro de 2019.
--- apyb/website/views.py
+++ apyb/website/views.py
@@ -3,6 +3,7 @@ import os
 from django.contrib.staticfiles.finders import find
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from django.views.generic import TemplateView
+from django_markup.markup import formatter
 
 HERO_ROOT = 'website/img/hero'
 HERO_PHOTOS = sorted([
@@ -19,4 +20,13 @@ class Home(TemplateView):
 
         context['hero_photos'] = HERO_PHOTOS
 
+        return context
+
+class About(TemplateView):
+    template_name = 'website/markdown.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(About, self).get_context_data(**kwargs)
+        markdown_content = open("website/markdown/about.md", 'r').read()
+        context['markdown_content'] = formatter(markdown_content, filter_name='markdown', safe_mode=False)
         return context
